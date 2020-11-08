e3f94b9e4c75150515c13f95bc29891513726b0b
Razer <trungnguyen1618033@gmail.com>
2020-11-08 14:07:39 +0700

    Update file getEmbedding.py

--- getEmbedding.py
+++ getEmbedding.py
@@ -6,48 +6,61 @@ import cv2
 import argparse
 import pickle
 from matplotlib import pyplot as plt
+from shutil import copyfile
 
-mtcnn_model= "./model/"
+
+
 arcface_model= "./model/model-r100-ii/model,0"
-ssh_detector="./model/ssh-model-final/sshb"
-gpu_index= 1
-mtcnn_num_worker=2
+retina_detector="./model/R50"
+gpu_index= 0
 
-model = FacialRecognition(mtcnn_model = mtcnn_model, arcface_model=arcface_model,
-                              ssh_detector=ssh_detector, gpu_index=gpu_index, mtcnn_num_worker=2)
+model = FacialRecognition(arcface_model=arcface_model,
+                              retina_model=retina_detector, gpu_index=gpu_index)
 flip = False
 
 dicts = []
-path_data = "Singer_3_train"
+path_data = './Data_Celeb/train'
 list_people = os.listdir(path_data)
-wr = open("singer-nganltp-model-3","wb")
+wr = open("Celeb-model-1","wb")
+
+path_else = './Data_Celeb/else'
+count = 0
 for person in list_people:
 #     os.mkdir('detect-face-100/' + str(person))
     print(person)
     path_person = os.path.join(path_data, person)
     list_name_person = os.listdir(path_person)
     error = 0
+
+    folder_else = os.path.join(path_else, person)
+    
     for name_img in list_name_person:
         try:
             dict = {}
             path_img = os.path.join(path_person, name_img)
             img = cv2.imread(path_img)
-            print(path_img)
+            print(path_img, count)
             embedding, nimg = model.detect_face_and_get_embedding(img)
+            count += 1
+            # print(embedding, nimg)
             if embedding is None:
                 error += 1
             if embedding is not None:
-                dict['class'] = person
-                dict['features'] = embedding
-                dict['imgfile'] = name_img
-                dicts.append(dict)
-    #         img = cv2.flip(img, 1)
-    #         embedding, nimg = model.detect_face_and_get_embedding(img)
-    #         if embedding is not None:
-    #             dict['class'] = person
-    #             dict['features'] = embedding
-    #             dict['imgfile'] = name_img
-    #             dicts.append(dict)
+                #print(len(embedding.shape))
+                if len(embedding) == 1:
+                  dict['class'] = person
+                  dict['features'] = embedding
+                  dict['imgfile'] = name_img
+                  dicts.append(dict)
+                if len(embedding.shape) != 1:
+                    if not os.path.exists(folder_else):
+                      os.makedirs(folder_else)
+                    else:    
+                      print("Directory already exists")
+                    print(os.path.join(path_else, person, name_img))
+                    copyfile(path_img, os.path.join(path_else, person, name_img))
+
+    #        
         except Exception as e:
             print('Caught this error: ' + repr(e))
 
